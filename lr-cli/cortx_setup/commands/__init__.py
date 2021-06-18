3ff53eda3ba338ec942618c3f6e7816700d262b3
Divya Vijayakumar <divya.vijaykumar@seagate.com>
2021-06-18 20:57:03 +0530

    EOS-21207: Enhancements after integration of Field APIs
    Signed-off-by: Divya Vijayakumar <divya.vijaykumar@seagate.com>

--- lr-cli/cortx_setup/commands/__init__.py
+++ lr-cli/cortx_setup/commands/__init__.py
@@ -22,7 +22,8 @@ from .node.initialize import NodeInitialize
 from .node.prepare.server import NodePrepareServer
 from .node.finalize import NodeFinalize
 from .cluster.create import ClusterCreate
-from .cluster.encrypt import EncryptPillar
+from .cluster.encrypt import EncryptSecrets
+from .cluster.generate import GenerateCluster
 from .cluster.show import ClusterShow
 from .cluster.config.network import ClusterNetworkConfig
 from .cluster.config.component import ClusterConfigComponent
@@ -55,7 +56,8 @@ __all__ = [
     'ClusterShow',
     'CreateStorageSet',
     'DurabilityConfig',
-    'EncryptPillar',
+    'EncryptSecrets',
+    'GenerateCluster',
     'GetSignature',
     'Hostname',
     'NetworkConfig',
