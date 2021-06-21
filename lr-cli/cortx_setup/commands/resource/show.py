52e743621e5c2a6e713e0574ad8a078c9655da20
sradhanandpati <sradhanand.pati@seagate.com>
2021-06-21 03:34:19 -0600

    EOS-21363: Generate healthview for specific resource with specific state
    Signed-off-by: sradhanandpati <sradhanand.pati@seagate.com>

--- lr-cli/cortx_setup/commands/resource/show.py
+++ lr-cli/cortx_setup/commands/resource/show.py
@@ -15,11 +15,22 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 #
 
-
+import json
 from ..command import Command
+from cortx_setup.commands.common_utils import get_pillar_data
 
 
 class ResourceShow(Command):
+
+    def parse_resource_file(self, resource_map_path: str):
+        file_type, file_path = resource_map_path.split("://")
+        if "json" in file_type:
+            with open(file_path,"r") as f:
+                return json.loads(f.read())
+        else:
+            raise Exception(f"Unsupported file type {file_type}")
+        
+
     _args = {
         'manefist': {
             'type': str,
@@ -32,12 +43,35 @@ class ResourceShow(Command):
             'default': None,
             'optional': True,
             'help': 'Health check for all the resources in the system'
-        }
+        },
+        'resource_type': {
+            'type': str,
+            'default': None,
+            'optional': True,
+            'help': 'Resource type for which resource map is to be fetched'
+        },
+        'resource_state': {
+            'type': str,
+            'default': None,
+            'optional': True,
+            'choices': ['OK', 'FAULT', 'DEGRADED'],
+            'help': 'The current state of the resources'
+        },
     }
 
-    def run(self, manefist=None, health=None):
-        if manefist:
-            self.logger.debug("discover HW/SW Manifest for all resources")
-        if health:
+    def run(self, **kwargs):
+
+        key = kwargs['resource_type'].split(">")
+
+
+
+        if kwargs["health"]:
+            resource_map_path = get_pillar_data('provisioner/common_config/resource_map_path')
+            
+            resource_dict = self.parse_resource_file(resource_map_path)
+
             self.logger.debug("Health check for all resources in the system")
+
+        else:
+            self.logger.debug("discover HW/SW Manifest for all resources")
         self.logger.debug("Done")
